5652c5c36d8c44c4c8bc8ea8894e3f2d60731389
programming <mehdi.developer.sh@gmail.com>
2021-02-14 19:09:08 +0330

    cleanup MainActivity.java imports

--- app/src/main/java/com/sky/plainnote2/MainActivity.java
+++ app/src/main/java/com/sky/plainnote2/MainActivity.java
@@ -6,7 +6,6 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
